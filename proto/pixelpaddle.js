ac87f666cf035d15e4a2b7a638952baf04b093e1
Josh <joshua.kernich@gmail.com>
2024-07-31 12:46:55 +0100

    Pixel pooling

--- proto/pixelpaddle.js
+++ proto/pixelpaddle.js
@@ -81,53 +81,57 @@ PixelPaddle = function(){
 
 	self.setPlayers([{px:15}]);
 
+	let pixels = [];
+	while(pixels.length<10) pixels.push($('<pixel>').appendTo($waterfall).css({'top':'150%'}));
+
 	let bpm = 81;
 	
 	let secondsPerBeat = 60/bpm;
 	
-
+	let long = 1.5;
+	let short = 1;
 
 	let beats = [
 
 		
-		{px:50,at:6,time:1.5},
-		{px:70,at:8,time:1.5},
-		{px:20,at:10,time:1.5},
-		{px:80,at:12,time:1.5},
-		{px:20,at:14,time:1.5},
-		{px:20,at:16,time:1.5},
-		{px:50,at:18,time:1.5},
-		{px:80,at:20,time:1.5},
-		{px:80,at:22,time:1.5},
+		{px:50,at:6,time:long},
+		{px:70,at:8,time:long},
+		{px:20,at:10,time:long},
+		{px:80,at:12,time:long},
+		{px:20,at:14,time:long},
+		{px:20,at:16,time:long},
+		{px:50,at:18,time:long},
+		{px:80,at:20,time:long},
+		{px:80,at:22,time:long},
 
-		{px:10,at:23,time:1.5},
-		{px:20,at:24,time:0.7},
-		{px:30,at:25,time:0.7},
-		{px:40,at:26,time:0.7},
-		{px:50,at:27,time:0.7},
-		{px:60,at:28,time:0.7},
-		{px:70,at:29,time:0.7},
-		{px:80,at:30,time:0.7},
-		{px:90,at:31,time:0.7},
+		{px:10,at:23,time:long},
+		{px:20,at:24,time:short},
+		{px:30,at:25,time:short},
+		{px:40,at:26,time:short},
+		{px:50,at:27,time:short},
+		{px:60,at:28,time:short},
+		{px:70,at:29,time:short},
+		{px:80,at:30,time:short},
+		{px:90,at:31,time:short},
 
-		{px:90,at:32,time:1.5},
-		{px:80,at:32,time:1.5},
+		{px:90,at:32,time:long},
+		{px:80,at:32,time:long},
 
-		{px:70,at:34,time:0.7},
-		{px:60,at:34,time:0.7},
+		{px:70,at:34,time:short},
+		{px:60,at:34,time:short},
 
-		{px:50,at:36,time:0.7},
-		{px:40,at:36,time:0.7},
+		{px:50,at:36,time:short},
+		{px:40,at:36,time:short},
 
-		{px:30,at:37,time:0.7},
-		{px:20,at:37,time:0.7},
+		{px:30,at:37,time:short},
+		{px:20,at:37,time:short},
 
 	];
 
 	let n = 40;
 	while(beats.length<100){
 		
-		beats.push({px:10 + Math.random() * 80,at:n,time:1.5});
+		beats.push({px:10 + Math.random() * 80,at:n,time:long});
 
 		n++;
 	}
@@ -137,8 +141,9 @@ PixelPaddle = function(){
 
 	let beatWas = 0;
 	let beatTravel = 3;
-	let pixels = [];
+	
 	let offset = 0.1;
+	let nPixel = 0;
 
 	function tick(){
 		
@@ -149,9 +154,9 @@ PixelPaddle = function(){
 
 		for(var b in beats){
 
-			if(!beats[b].$el && timeNow > beats[b].timeSpawn) beats[b].$el = $('<pixel>').appendTo($waterfall);
+			if(!beats[b].dead && !beats[b].$el && timeNow > beats[b].timeSpawn) beats[b].$el = pixels[ (nPixel++)%pixels.length ]
 
-			if(beats[b].$el){
+			if(!beats[b].dead && beats[b].$el){
 
 				if(beats[b].gravity == undefined){
 					beats[b].progress = 1 + (timeNow - beats[b].timeSpawn - beats[b].time)/beats[b].time;
@@ -182,9 +187,12 @@ PixelPaddle = function(){
 
 					beats[b].sy += beats[b].gravity;
 					beats[b].progress += beats[b].sy;
-
-					console.log(beats[b].sy,beats[b].progress);
 					beats[b].px += beats[b].sx;
+
+					if( beats[b].progress > 1.5 ){
+						beats[b].$el.css({'background':'white'});
+						beats[b].dead = true;
+					}
 				}
 				
 
@@ -197,68 +205,7 @@ PixelPaddle = function(){
 
 		
 
-		if(beat>beatWas){
-
-			/*for(var p in pixels){
-			
-				if(pixels[p].active){
-					pixels[p].beat--;
-				} else if(pixels[p].bouncing) {
-					pixels[p].beat += pixels[p].sy;
-					pixels[p].sy += 0.1;
-					pixels[p].px += pixels[p].sx;
-
-				}
-
-				if(pixels[p].beat==-1){
-				 
-				 	for(var n in players){
-				 		let dist = players[n].px - pixels[p].px;
-
-				 		 
-
-				 		if(dist < 10 && dist > -10 ){
-				 			pixels[p].beat = 0;
-				 			pixels[p].$el.css('background','red');
-				 			pixels[p].active = false;
-				 			pixels[p].sx = -dist;
-				 			pixels[p].sy = 1;
-				 			pixels[p].bouncing = true;
-
-				 			pixels[p].$el.css({
-								'transition':'none',
-							});
-				 		}
-				 	}
-
-				 	
-				}
-				if(pixels[p].beat==-2){
-					pixels[p].active = false;
-					pixels[p].bouncing = false;
-				}
-			}
-
-
-
-			if(beats.indexOf(beat)>-1){
-				let $p = $('<pixel>').appendTo($front);
-
-				pixels.push({$el:$p,beat:beatTravel,px:Math.random()*100,py:0, active:true});
-			}*/
-		}
-
-		/*beatWas = beat;
-
-
-		for(var p in pixels){
-			
-			pixels[p].$el.css({
-				'left':pixels[p].px+'%',
-				'bottom':1 + pixels[p].beat*6+'vw'
-			});
-		}*/
-
+		
 		
 			
 	}
