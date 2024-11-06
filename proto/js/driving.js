3c5a514e37f201dec8da8bee5d9ad7296d19b139
Josh <joshua.kernich@gmail.com>
2024-11-06 15:55:19 +0000

    Turning

--- proto/js/driving.js
+++ proto/js/driving.js
@@ -15,7 +15,7 @@ Box3D = function(w,l,h,color){
 				transform-style: preserve-3d;
 				text-align:center;
 
-				border: 1px solid white;
+				
 			}	
 
 			box3Dside.box3D-back{
@@ -63,7 +63,7 @@ Box3D = function(w,l,h,color){
 
 DrivingGame = function(){
 
-	const FPS = 30;
+	const FPS = 40;
 	const W = 400;
 	const H = 250;
 	const GRID = W;
@@ -82,34 +82,31 @@ DrivingGame = function(){
 				height:${H}px;
 				box-sizing:border-box;
 				transform-origin:top left;
-				perspective:${W}px;
 				background: black;
 			}
 
-			drivinggame:before{
-				content:"";
-				width:1px;
-				height:100%;
-				background:red;
-				position:absolute;
-				left:0px;
-				top:0px;
+			drivingviewport{
 				display:block;
-				z-index: 100;
+				position: absolute;
+				top:0px;
+				left: 0px;
+				perspective:${W}px;
+				width:${W}px;
+				height:${H}px;
+
+				overflow: hidden;
+				background: black;
 			}
 
-			drivinggame:after{
-				content:"";
-				width:1px;
-				height:100%;
-				background:red;
-				position:absolute;
-				right:0px;
-				top:0px;
+			drivingcamera{
 				display:block;
-				z-index: 100;
+				position: absolute;
+				width:${W}px;
+				height:${H}px;
+				transform-style: preserve-3d;
 			}
 
+		
 			drivingworld{
 				display:block;
 				box-sizing:border-box;
@@ -159,7 +156,7 @@ DrivingGame = function(){
 				height:${LCAR*GRID}px;
 				transform: translateZ(5px);
 				transform-style: preserve-3d;
-				background: red;
+				background: black;
 
 				
 				
@@ -186,18 +183,28 @@ DrivingGame = function(){
 			}
 
 			drivinglayer{
+				
+				position:absolute;
+				width:0px;
+				height:0px;
+				transform-style: preserve-3d;
+			}
+
+			drivinglayer:after{
+				content:"";
 				display:block;
 				position:absolute;
 				width:${GRID*3}px;
-				height:${GRID}px;
-				
+				height:${GRID*1.1}px;
 				background-image: url(./proto/img/bat-road.png);
 				background-size: 100%;
 				transform-style: preserve-3d;
-			
-				left: 0px;
+				left: ${-GRID*3/2}px;
+				bottom: 0px;
 			}
 
+
+			
 
 		</style>
 	`);
@@ -222,8 +229,13 @@ DrivingGame = function(){
 	let $right = $('<igbside>').appendTo(self.$el);
 
 	let $game = $('<drivinggame>').appendTo($front);
-	let $world = $('<drivingworld>').appendTo($game);
+
+	let $viewport = $('<drivingviewport>').appendTo($game);
+	let $camera = $('<drivingcamera>').appendTo($viewport);
+
+	let $world = $('<drivingworld>').appendTo($camera);
 	let $plane = $('<drivingplane>').appendTo($world);
+	
 
 	let $car = $(`<drivingcar>`).appendTo($plane);
 	let $i = $(`<drivingcarinner>`).appendTo($car);
@@ -232,11 +244,7 @@ DrivingGame = function(){
 
 	$layers = [];
 	
-	for(var i=0; i<20; i++){
-		let $layer = $('<drivinglayer>').appendTo($plane);
-
-		$layers[i] = $layer;
-	}
+	
 
 	/*for(var t in TRACK){
 		
@@ -266,86 +274,93 @@ DrivingGame = function(){
 
 	let players = [{px:0}];
 	let iTick = 0;
-	let speed = 2.5/FPS;
+	let speed = 4/FPS;
 	let steer = FPS*0.2;
-	let xCenter = 2.5;
+	let xCenter = 0;
 
 	let iyTrack = -1;
 
-	let curve = [{x:0,r:0}];
+	let curve = [{x:0,y:0,r:0}];
+
+	function add(deg){
+		let r = curve[curve.length-1].r;
+		let x = curve[curve.length-1].x + Math.sin(r + deg);
+		let y = curve[curve.length-1].y + Math.cos(r + deg);
+
+		curve[curve.length-1].skew = deg;
+
+		curve.push({x:x,y:y,r:r+deg,skew:0});
+	}
 
 	function addStraight(count) {
-		let xAnchor = curve[curve.length-1].x;
-		while(count){
-			count--;
-			curve.push({x:xAnchor,r:0});
-		}
+		let n = 0;
+		while(count--) add(0);
 	}
 
 	function addSlide(count,dir) {
-		let xAnchor = curve[curve.length-1].x;
-
-		while(count){
-			count--;
-			xAnchor += 0.4*dir;
-			curve.push({x:xAnchor,r:dir*0.4});
-		}
+		while(count--) add(0.05*dir);
 	}
 
-	while(curve.length<200){
+	while(curve.length<1000){
 		addStraight(5+Math.floor(Math.random()*5));
-		addSlide(5+Math.floor(Math.random()*5),(Math.random()>0.5?1:-1));
+		addSlide(10+Math.floor(Math.random()*5),(Math.random()>0.5?1:-1));
 	}
+
+
+	for(var l=0; l<15; l++) $layers[l] = $('<drivinglayer>').appendTo($plane);
 	
 	
+
+	let car = {x:0,y:0,r:0};
+
+	let iRender = 0;
 
 	function tick(){
 		let w = $(document).innerWidth()/3;
 		$game.css('transform','scale('+(w/W)+')');
 
-		iTick++;
-		
-		let prog = speed*iTick;
-
-		let txRelative = (players[0].px/100-0.5);
-		let txActual = xCenter + txRelative;
-
-		let xLeft = txActual - WCAR/2;
-		let xRight = txActual + WCAR/2;
-
-		let ixLeft = Math.floor(xLeft);
-		let ixRight = Math.floor(xRight);
-		let iyProgress = Math.floor(prog)-1;
+		let txRelative = (players[0].px/100*2-1);
 
 
-		while(iyTrack<(iyProgress+$layers.length)){
+		car.x += Math.sin(car.r*Math.PI/180)*speed;
+		car.y += Math.cos(car.r*Math.PI/180)*speed;
 
-			iyTrack++;
-			let n = iyTrack%$layers.length;
-			$layers[n].css({
-				'bottom':(iyTrack*GRID)+'px',
-				'left':curve[iyTrack].x*GRID+'px',
-				'transform':'rotate('+curve[iyTrack].r*55+'deg)',
-			});
+
+		if(iRender<curve.length){
+			let dx = curve[iRender].x - car.x;
+			let dy = curve[iRender].y - car.y;
+			let d = Math.sqrt(dx*dx+dy*dy);
+
+			if(d<12){
+				iRender++;
+				let n = iRender%$layers.length;
+				$layers[n].css({
+					'bottom':curve[iRender].y*GRID+'px',
+					'left':curve[iRender].x*GRID+'px',
+					'transform':'rotate('+curve[iRender].r*180/Math.PI+'deg) skew('+-curve[iRender].skew*180/Math.PI+'deg)',
+				});
+			}
 		}
+		
+		
 
-		xCenter = (xCenter*(steer-1) + txActual)/steer;
+		let r = txRelative*45;
+		car.r += r*0.02;
 
-		let r = txRelative*90;
 
 		$car.css({
-			left:txActual*GRID +'px',
-			bottom: (prog+0.1)*GRID + 'px',
-			'transform':'rotateZ('+r+'deg)',
-		})
-
-		$plane.css({
-			left:-xCenter*GRID+'px',
-			bottom:-prog*GRID+'px',
+			left: car.x*GRID +'px',
+			bottom: car.y*GRID + 'px',
+			'transform':'rotateZ('+(car.r+r)+'deg)',
 		})
 
 
+		let cx = Math.sin(car.r*Math.PI/180)*0.25;
+		let cy = Math.cos(car.r*Math.PI/180)*0.25;
 		
+	    $camera.css('transform',"rotateY(" + car.r + "deg)");
+	   	$plane.css('bottom',-(car.y-cy)*GRID+'px');
+	   	$plane.css('left',-(car.x-cx)*GRID+'px');
 		
 	}
 
