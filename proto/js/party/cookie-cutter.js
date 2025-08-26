893b87c0a1bc0c4a7e17a861b251fe1c6210220a
Josh <joshua.kernich@gmail.com>
2025-08-27 09:44:08 +1000

    Cookie cutter queue and scoring

--- proto/js/party/cookie-cutter.js
+++ proto/js/party/cookie-cutter.js
@@ -9,6 +9,37 @@ window.CookieCutterGame = function(){
 
 	const GRID = {W:W/GRIDSIZE,H:TRACKLENGTH};
 
+	const STRUCTURE = [
+		undefined,
+		undefined,
+		[
+			{players:[0,1],levels:10},
+			{players:[0,1],levels:30}
+		],
+		[
+			{players:[0,1,2],levels:10},
+			{players:[0,1,2],levels:30}
+		],
+		[
+			{players:[0,1,2,3],levels:10},
+			{players:[0,1,2,3],levels:30}
+		],
+		[
+			{players:[0,1,2],levels:10},
+			{players:[3,4,0],levels:10},
+			{players:[1,2,3],levels:10},
+			{players:[4,0,1],levels:10},
+			{players:[2,3,4],levels:10},
+		],
+		[
+			{players:[0,1,2],levels:10},
+			{players:[3,4,5],levels:10},
+			{players:[0,2,4],levels:20},
+			{players:[1,3,5],levels:20},
+		],
+
+	]
+
 	if(!MazeGame.didInit){
 		MazeGame.didInit = true;
 
@@ -43,17 +74,16 @@ window.CookieCutterGame = function(){
 					position: absolute;
 					left: 0px;
 					right: 0px;
-					bottom: 100px;
+					bottom: 110px;
 					margin: auto;
 					transform: rotateX(70deg);
 					z-index: 10;
 					transform-style:preserve-3d;
 					border-radius: 400px;
 
-					
-
-					background: #EEB067;
-					box-shadow: 0px 100px 0px #773E0F;
+		
+					background: url(./proto/img/party/texture-wood.png);
+					box-shadow: 0px 50px 0px #422a23;
 
 				}
 
@@ -81,7 +111,8 @@ window.CookieCutterGame = function(){
 					overflow: hidden;
 					border-radius: 400px;
 					transition: all 0.5s;
-					border-bottom: 15px solid black;
+					border-bottom: 50px solid black;
+					box-shadow: inset 0px 0px 100px white;
 				}
 
 				cookiecorner{
@@ -148,7 +179,25 @@ window.CookieCutterGame = function(){
 					border: none;
 				}
 
+				cookiemeep{
+					display: block;
+					position: absolute;
+					transform-style:preserve-3d;
+					transform: rotateX(-90deg);
+				}
 
+				cookiescore{
+					display: block;
+					position: absolute;
+					bottom: 300px;
+					line-height: 100px;
+					font-size: 75px;
+					color: white;
+					left: -50px;
+					right: -50px;
+					text-align: center;
+					text-shadow: 5px 0px 5px #333, 0px 5px 5px #333, 0 -5px 5px #333, -5px 0px 5px #333;
+				}
 
 			</style>`);
 	}
@@ -157,6 +206,37 @@ window.CookieCutterGame = function(){
 	audio.add('music','./proto/audio/party/music-creeping.mp3',0.3,true);
 	audio.add('squish','./proto/audio/party/sfx-squish.mp3',0.3);
 
+	const CookieMeep = function(n){
+		let self = this;
+		self.$el = $('<cookiemeep>');
+
+		self.score = 0;
+
+		let meep = new PartyMeep(n);
+		meep.$el.appendTo(self.$el);
+		meep.$el.css({
+			'transform':'scale(0.8)',
+		});
+
+		let $score = $('<cookiescore>').appendTo(self.$el);
+
+		self.addScore = function(score){
+			self.score += score;
+			$score.text(self.score)
+			.stop(false,false)
+			.css({
+				opacity:1,
+				bottom: 250,
+			}).animate({
+				bottom: 350,
+			},200).animate({
+				bottom: 300,
+			},200).delay(700).animate({
+				opacity:0
+			})
+		}
+	}
+
 	let self = this;
 	self.$el = $('<igb>');
 
@@ -164,29 +244,49 @@ window.CookieCutterGame = function(){
 	let $world = $('<cookieworld>').appendTo($game);
 	let $center = $('<cookiecenter>').appendTo($world);
 	
-
+	let iRound = -1;
 	let meeps = [];
 	function initGame(PLAYER_COUNT){
 
+		
 		scoreMult = Math.floor( SCORE_MAX / PLAYER_COUNT );
 
 		for(var i=0; i<PLAYER_COUNT; i++){
 
-			meeps[i] = new PartyMeep(i);
-
-			meeps[i].setHeight(350);
-			meeps[i].$el.appendTo($center);
-			meeps[i].$el.css({
-				'transform-style':'preserve-3d',
-				transform: 'rotateX(-60deg) scale(0.8)',
-			});
+			meeps[i] = new CookieMeep(i);
+			meeps[i].isActive = false;
+			meeps[i].$el.appendTo($center).hide();
+			
 		}
 
 		audio.play('music');
-		//
+
+		initNextRound();
+	}
+
+	function getNonPlayers(players){
+		let arr = [];
+		for(var m=0; m<meeps.length; m++) if(players.indexOf(m)==-1) arr.push(m);
+		return arr;
+	}
+
+	function initNextRound(){
+
+		iRound++;
+
+		for(var m in meeps) meeps[m].isActive = false;
+
+		for(var p in STRUCTURE[meeps.length][iRound].players){
+
+			let m = STRUCTURE[meeps.length][iRound].players[p];
+
+			meeps[m].$el.show();
+			meeps[m].isActive = true;
+			meeps[m].isDead = false;
+		}
 
 		setTimeout(function(){
-			hud.initBanner('Ready...');
+			hud.initRound('Round '+(iRound+1), iRound, STRUCTURE[meeps.length].length);
 		},2000);
 
 		setTimeout(function(){
@@ -194,13 +294,21 @@ window.CookieCutterGame = function(){
 		},4000);
 
 		setTimeout(function(){
-			hud.initBanner('Go!');
+			hud.summonPlayers(STRUCTURE[meeps.length][iRound].players,getNonPlayers(STRUCTURE[meeps.length][iRound].players));
 		},6000);
 
 		setTimeout(function(){
 			hud.finiBanner();
-			doUnstamp();
 		},8000);
+
+		setTimeout(function(){
+			hud.initBanner('Dodge the Cookie Cutter!');
+		},10000);
+
+		setTimeout(function(){
+			hud.finiBanner();
+			doUnstamp();
+		},12000);
 	}
 	
 
@@ -234,10 +342,12 @@ window.CookieCutterGame = function(){
 		resize();
 
 		for(var m in meeps){
-			meeps[m].$el.css({
-				left: meeps[m].x + 'px',
-				top: meeps[m].y + 'px',
-			})
+			if(meeps[m].isActive){
+				meeps[m].$el.css({
+					left: meeps[m].x + 'px',
+					top: meeps[m].y + 'px',
+				})
+			}
 		}
 	}
 
@@ -251,9 +361,11 @@ window.CookieCutterGame = function(){
 		[0,1,1,0],
 		[0,0,0,1],
 		[1,0,0,0],
+		[0,1,0,0],
+		[0,0,1,0],
 	]
-	let nStamp = -1;
 
+	let nStamp = -1;
 
 	function doNewStamp(){
 		nStamp++;
@@ -276,12 +388,15 @@ window.CookieCutterGame = function(){
 			'opacity':'0.5',
 		})
 
-		setTimeout(doStamp,Math.max(500, 1500 - nStamp*100));
+		let time = 1500 - nStamp*100;
+		if(nStamp == 0) time = 2000; // always take 2 seconds for the first stamp
+
+		setTimeout(doStamp,Math.max(500, time));
 	}
 
 	function doStamp() {
 		$stamper.css({
-			'transform':'translateZ(0px)',
+			'transform':'translateZ(30px)',
 		})
 
 		$shadow.css({
@@ -297,49 +412,71 @@ window.CookieCutterGame = function(){
 	function doCrush(){
 		let stamp = STAMPS[nStamp%STAMPS.length];
 		for(var m in meeps){
-			let isLeft = meeps[m].x < 0;
-			let isTop = meeps[m].y < 0;
+			if(meeps[m].isActive){
+				let isLeft = meeps[m].x < 0;
+				let isTop = meeps[m].y < 0;
 
-			let isMiddle = ( Math.abs( meeps[m].x ) < 75 ) || ( Math.abs( meeps[m].y ) < 75 );
-			let isEdge = ( Math.abs( meeps[m].x ) > (W/2-75) ) || ( Math.abs( meeps[m].y ) > (W/2-75) );
+				let isMiddle = ( Math.abs( meeps[m].x ) < 75 ) || ( Math.abs( meeps[m].y ) < 75 );
+				let isEdge = ( Math.abs( meeps[m].x ) > (W/2-75) ) || ( Math.abs( meeps[m].y ) > (W/2-75) );
 
-			let iQuadrant = 0;
-			if( !isLeft && isTop ) iQuadrant = 1;
-			if( isLeft && !isTop ) iQuadrant = 2;
-			if( !isLeft && !isTop ) iQuadrant = 3;
+				let iQuadrant = 0;
+				if( !isLeft && isTop ) iQuadrant = 1;
+				if( isLeft && !isTop ) iQuadrant = 2;
+				if( !isLeft && !isTop ) iQuadrant = 3;
 
-			if(!meeps[m].dead && (isMiddle || isEdge || stamp[iQuadrant]==0)){
-				meeps[m].$el.hide();
-				meeps[m].dead = true;
-				audio.play('squish',true);
+				if(!meeps[m].isDead && (isMiddle || isEdge || stamp[iQuadrant]==0)){
+					meeps[m].$el.hide();
+					meeps[m].isDead = true;
+					audio.play('squish',true);
 
-				countSquish++;
-				meeps[m].score = countSquish * scoreMult;
+					countSquish++;
+					
+				} else {
+					meeps[m].addScore( 1 );
+				}
 			}
 		}
 
 		let countAlive = 0;
-		for(var m in meeps) if(!meeps[m].dead) countAlive++;
+		for(var m in meeps) if(!meeps[m].isDead && meeps[m].isActive) countAlive++;
 
 		if(countAlive<=1){
 
-			for(var m in meeps) if(!meeps[m].dead) meeps[m].score = SCORE_MAX;
+			for(var m in meeps) if(!meeps[m].isDead && meeps[m].isActive) meeps[m].addScore( STAMPS.length-nStamp-1 );
 
 			clearTimeout(timer);
+			setTimeout( function(){ doUnstamp(false) }, 1000 );
 			$stamper.stop(false);
 			hud.initBanner(countAlive==1?'Winner!':'Finish!');
 
-			setTimeout(function(){
-				let scores = [];
-				for(var m in meeps) scores[m] = meeps[m].score;
+			setTimeout( hud.finiBanner, 3000 );
 
-				self.fini();
-				window.doPartyGameComplete(scores);
-			},3000)
+			if(STRUCTURE[meeps.length][iRound+1]){
+				setTimeout( initNextRound, 3500 );
+			} else {
+				setTimeout( finiGame, 3500 );
+			}
 		}
 	}
 
-	function doUnstamp() {
+	function finiGame(){
+		let scores = [];
+		for(var m in meeps){
+			meeps[m].isActive = false;
+			meeps[m].$el.hide();
+			scores[m] = meeps[m].score;
+		}
+
+		let rewards = scoresToRewards(scores);
+		hud.showFinalScores(scores,rewards);
+
+		setTimeout(function(){
+			self.fini();	
+			window.doPartyGameComplete(rewards);
+		},3000);
+	}
+
+	function doUnstamp( doNextStamp=true ) {
 		$stamper.css({
 			'transform':'translateZ('+H+'px)',
 		})
@@ -348,7 +485,7 @@ window.CookieCutterGame = function(){
 			'opacity':'0',
 		})
 
-		setTimeout(doNewStamp,1000);
+		if( doNextStamp ) setTimeout(doNewStamp,1000);
 	}
 
 	let interval = setInterval(step,1000/FPS);
--- proto/js/shared/shared.js
+++ proto/js/shared/shared.js
@@ -13,3 +13,27 @@ window.shuffleArray = function(array) {
       array[randomIndex], array[currentIndex]];
   }
 }
+
+window.scoresToRewards = function(scores){
+
+  const SCORES = [
+    undefined,
+    undefined,
+    [20,10],
+    [20,10,5],
+    [20,10,5,2],
+    [20,15,10,5,2],
+    [20,17,12,10,5,2],
+  ];
+
+  let standings = [];
+  for(var a=0; a<scores.length; a++){
+    standings[a] = scores.length;
+    for(var b=0; b<scores.length; b++) if(scores[a] >= scores[b]) standings[a]--;
+  }
+
+  let rewards = [];
+  for(var s=0; s<standings.length; s++) rewards[s] = SCORES[scores.length][ standings[s] ];
+
+  return rewards;
+}
